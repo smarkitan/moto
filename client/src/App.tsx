c9c4b93776d3ded22b5d0b92834f0afc9801c89d
smarkitan <97811066+smarkitan@users.noreply.github.com>
2023-11-14 13:14:50 +0200

    ok

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -66,7 +66,7 @@ function App() {
 
             if (profileObj) {
                 const response = await fetch(
-                    "https://moto-album.onrender.com/api/v1/users",
+                    "https://dashed.onrender.com/api/v1/users",
                     {
                         method: "POST",
                         headers: { "Content-Type": "application/json" },
@@ -137,7 +137,7 @@ function App() {
             <GlobalStyles styles={{ html: { WebkitFontSmoothing: "auto" } }} />
             <RefineSnackbarProvider>
                 <Refine
-                    dataProvider={dataProvider("https://moto-album.onrender.com/api/v1")}
+                    dataProvider={dataProvider("https://dashed.onrender.com/api/v1")}
                     notificationProvider={notificationProvider}
                     ReadyPage={ReadyPage}
                     catchAll={<ErrorComponent />}
