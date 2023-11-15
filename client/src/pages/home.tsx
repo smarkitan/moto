4e8234fea4464e75b8ae6226519aa23fabb6eea3
smarkitan <97811066+smarkitan@users.noreply.github.com>
2023-11-15 15:42:11 +0200

    small changes

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,32 +1,20 @@
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
-import Stack from "@mui/material/Stack";
+
 
 import {hdlogo} from "../assets";
 
 const home = () => {
   return (
-    <Box>
-    <Typography fontSize={25} fontWeight={700} color="#11142D">
-       
-    </Typography>
-    <Typography fontSize={25} fontWeight={700} color="#11142d">
+         <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'flex-start', minHeight: '100vh' }}>
+            <Typography fontSize={25} fontWeight={700} color="#11142d" style={{ textAlign: 'center' }}>
                 My passion
-                </Typography>
+            </Typography>
 
-    <Stack
-                mt="25px"
-                width="100%"
-                direction={{ xs: "column", lg: "row" }}
-                gap={4}
-            >
-            <Box mt="20px" display="flex" flexWrap="wrap" gap={4}>
-              <div style={{ maxWidth: '200px', maxHeight: '200px' }}>
-              <img src={hdlogo} alt="HD Logo" style={{ maxWidth: '100%', maxHeight: '100%' }} />
-              </div>
-              </Box>
-       </Stack>
-    </Box>
+            <Box mt="25px">
+                <img src={hdlogo} alt="HD Logo" style={{ maxWidth: '200px', maxHeight: '200px' }} />
+            </Box>
+        </div>
   )
 }
 
